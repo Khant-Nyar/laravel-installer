3f87071da61ee76bfb0fce77327e82bca8feb9f8
khantsithuphyo2001 <khantsithuphyo2001@gmail.com>
2025-03-16 22:47:44 +0000

    :init

---
+++ dev.nix
@@ -0,0 +1,56 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+    # Which nixpkgs channel to use.
+    channel = "stable-24.05"; # or "unstable"
+    # Use https://search.nixos.org/packages to find packages
+    packages = [
+        pkgs.php83
+        pkgs.php83Packages.composer
+        pkgs.nodejs_20
+        pkgs.mariadb
+    ];
+    
+    # Enable services like PostgreSQL and Docker
+    services = {
+        docker.enable = true;
+        mysql.enable = true;
+    };
+
+    # Sets environment variables in the workspace
+    env = {
+
+    };
+
+    idx = {
+        # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+        extensions = [
+        # "vscodevim.vim"
+        ];
+        # Enable previews
+        previews = {
+            enable = true;
+            previews = {
+                web = {
+                    # command = [ "composer" "run" "dev" ];
+                    command = ["php" "artisan" "serve" "--port" "$PORT" "--host" "0.0.0.0"];
+                    manager = "web";
+                };
+            };
+        };
+        # Workspace lifecycle hooks
+        workspace = {
+        # Runs when a workspace is first created
+            onCreate = {
+                # Example: install JS dependencies from NPM
+                # npm-install = "npm install";
+                default.openFiles = [ ".idx/dev.nix" "README.md" ];
+            };
+            # Runs when the workspace is (re)started
+            onStart = {
+                # Example: start a background task to watch and re-build backend code
+                # watch-backend = "npm run watch-backend";
+            };
+        };
+    };
+}
---
+++ idx-template.nix
@@ -0,0 +1,25 @@
+{ pkgs, version ? "latest", ... }: {
+	
+    packages = [
+        pkgs.php83
+        pkgs.php83Packages.composer
+        pkgs.nodejs_20
+        pkgs.mariadb
+    ];
+    services = {
+        docker.enable = true;
+        mysql.enable = true;
+    };
+
+    bootstrap = ''
+        mkdir composer-home
+        export COMPOSER_HOME=./composer-home
+			mkdir "$out"
+        composer global require laravel/installer
+        export PATH=~/.config/composer/vendor/bin:$PATH
+        laravel new "$out"
+        composer create-project laravel/laravel "$out"
+			mkdir -p "$out"/.idx
+  		cp ${./dev.nix} "$out"/.idx/dev.nix
+    '';
+}
